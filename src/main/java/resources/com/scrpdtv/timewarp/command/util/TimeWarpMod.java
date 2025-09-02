0fa83717db1b154c31e3408b30b2b683d2373226
scrpdtv <scrpdtv@gmail.com>
2025-09-02 23:45:32 +0200

    Update TimeWarpMod.java

--- src/main/java/resources/com/scrpdtv/timewarp/command/util/TimeWarpMod.java
+++ src/main/java/resources/com/scrpdtv/timewarp/command/util/TimeWarpMod.java
@@ -1,1 +1,14 @@
-h
+package com.scrpdtv.timewarp;
+
+import net.fabricmc.api.ModInitializer;
+import com.scrpdtv.timewarp.command.TimeWarpCommand;
+
+public class TimeWarpMod implements ModInitializer {
+    public static final String MOD_ID = "timewarp";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[TimeWarpMod] Mod initialized!");
+        TimeWarpCommand.register();
+    }
+}
